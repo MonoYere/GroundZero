49f071020b958650296e42dc2e4a3d7df558e3c9
MonoYere <111619419+MonoYere@users.noreply.github.com>
2023-06-22 19:10:42 -0400

    DB.01
    --Sé arregló error de migraciones

--- appgroundzero/models.py
+++ appgroundzero/models.py
@@ -1,4 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Artista(models.Model):
+    id_artista = models.AutoField(db_column='idArtista', primary_key=True)
+    nmb_artista = models.CharField(max_length=120)
 
+    def __str__(self):
+        return str(self.nmb_artista)
+
+
+class Categoria(models.Model):
+    id_categoria = models.AutoField(db_column='idCategoria', primary_key=True)
+    nmb_categoria = models.CharField(max_length=120)
+
+    def __str__(self):
+        return str(self.nmb_categoria)
+
+
+class Producto(models.Model):
+    id_producto = models.AutoField(primary_key=True)
+    nmb_producto = models.CharField(max_length=120)
+    fch_creacion = models.DateField(blank=False, null=False)
+    id_categoria = models.ForeignKey('Categoria', on_delete=models.CASCADE, db_column='idCategoria')
+    id_artista = models.ForeignKey('Artista', on_delete=models.CASCADE, db_column='idArtista')
+
+    def __str__(self):
+        return str(self.nmb_producto)
